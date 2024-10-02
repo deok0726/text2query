fcaf5d28ad3a4347379ac849d9b9c236ae018b31
deok0726 <deok0726@gmail.com>
2024-10-02 20:27:27 +0900

    Make new db for test

--- App/nnn.py
+++ App/nnn.py
@@ -13,7 +13,23 @@ import sqlite3
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def sql_result(db, question):
+def generate_natural_language_answer(llm, question, sql_query, sql_result):
+    # 프롬프트 템플릿 정의
+    answer_prompt_template = '''
+    Given the following user question, corresponding SQL query, and SQL result, answer the user question in natural language in Korean.
+
+    Question: {question}
+    SQL Query: {sql_query}
+    SQL Result: {sql_result}
+
+    Answer: '''
+    
+    answer_prompt = answer_prompt_template.format(question=question, sql_query=sql_query, sql_result=sql_result)
+    response = llm(answer_prompt)
+    
+    return response
+
+def sql_result(llm, db, question):
     few_shot_examples = '''
         Example 1)
         Question: LG전자에서 사용한 총 금액을 알려줘
@@ -27,6 +43,7 @@ def sql_result(db, question):
         SQLResult: 3
         Answer: 지금까지 치킨을 5 번 시켜먹었습니다.
     '''
+
     template = '''Given an input question, create a syntactically correct top {top_k} {dialect} query to run which should end with a semicolon.
         Use the following format:
 
@@ -55,40 +72,24 @@ def sql_result(db, question):
         result = execute.invoke({"query": generated_sql_query})
         print("-"*200)
         print(result)
+
+        answer = generate_natural_language_answer(llm, question, generated_sql_query, result)
+        print(answer)
+
         # result = execute.invoke({"query": "SELECT T1.MC_NM, T2.APR_DTTI, T2.CDNO, T2.DE_DT, T2.CNO, T2.APR_DE_AM FROM LM_TB_MC AS T1 INNER JOIN LP_TB_TBAUTH AS T2 ON T1.MCNO = T2.MCNO WHERE T1.MC_NM = '스타벅스'"})
-    except:
+    except Exception as e:
        print("-"*200)
+       print(f"Error: {e}")
        print(generated_sql_query.__repr__())
 
-def sql_answer(db, question):
-    execute_query = QuerySQLDataBaseTool(db=db)
-    write_query = create_sql_query_chain(llm, db)
-
-    answer_prompt = PromptTemplate.from_template(
-    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
-
-            Question: {question}
-            SQL Query: {query}
-            SQL Result: {result}
-            Answer: """
-    )
-
-    chain = (
-        RunnablePassthrough.assign(query=write_query).assign(
-            result=itemgetter("query") | execute_query
-        )
-        | answer_prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    chain.invoke({f"question": {question}})
 
 if __name__ == "__main__":
     db_name = input("Input DB name: ")
     llm = Ollama(model="llama3.1:70b", temperature=0)
     # llm = Ollama(model="codellama:70b", temperature=0)
     
+    
+
     db = SQLDatabase.from_uri(f"sqlite:///{db_name}.db")
     print(db.dialect)
     print(db.get_usable_table_names())
@@ -96,5 +97,4 @@ if __name__ == "__main__":
     question = input("DB 질문을 입력하세요: ")
     # question = "스타벅스에서 결제한 전체 내역을 보여줘"
 
-    sql_result(db, question)
-    sql_answer(db, question)
+    sql_result(llm, db, question)
